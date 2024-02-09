09411606b30c566bcb6f8331c038d3dc8adb3223
Nathan Ventura <nathanventura.dev@gmail.com>
2024-02-09 02:29:23 -0300

    Add Addition

--- Program.cs
+++ Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Calculator
+
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         Soma();
+      }
+      static void Soma()
+      {
+         Console.Clear();
+         Console.WriteLine("Primeiro valor: ");
+         float v1 = float.Parse(Console.ReadLine());
+
+         Console.WriteLine("Segundo valor: ");
+         float v2 = float.Parse(Console.ReadLine());
+
+         float total = v1 + v2;
+
+         Console.WriteLine("");
+
+         Console.WriteLine($"A soma dos valores é {total}");
+      }
+   }
+}
