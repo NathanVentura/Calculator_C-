13dc190097ba82369d333af6932819dc94e6f4e8
Nathan Ventura <nathanventura.dev@gmail.com>
2024-02-11 18:37:23 -0300

    adding the Multiplication method to the calculator project

--- Program.cs
+++ Program.cs
@@ -10,6 +10,7 @@ namespace Calculator
          Soma();
          Subtracao();
          Divisao();
+         Multiplicacao();
       }
       static void Soma()
       {
@@ -58,6 +59,25 @@ namespace Calculator
          float resultado = v1 / v2;
 
          Console.WriteLine($"O resultado da Divisão é {resultado} ");
+
+         Console.ReadKey();
+
+      }
+      static void Multiplicacao()
+      {
+         Console.Clear();
+
+         Console.WriteLine("Primeiro valor");
+         float v1 = float.Parse(Console.ReadLine());
+
+         Console.WriteLine("Segundo valor");
+         float v2 = float.Parse(Console.ReadLine());
+
+         Console.WriteLine("");
+
+         float resultado = v1 * v2;
+
+         Console.WriteLine($"O resultado da multiplicação é {resultado}");
 
          Console.ReadKey();
 
