a5690e13343046e3cada35c7cd05b1bdd15c2035
Nathan Ventura <nathanventura.dev@gmail.com>
2024-02-11 17:53:22 -0300

    adding the subtraction method to the calculator project

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ namespace Calculator
       static void Main(string[] args)
       {
          Soma();
+         Subtracao();
       }
       static void Soma()
       {
@@ -18,11 +19,30 @@ namespace Calculator
          Console.WriteLine("Segundo valor: ");
          float v2 = float.Parse(Console.ReadLine());
 
-         float total = v1 + v2;
+         float resultado = v1 + v2;
 
          Console.WriteLine("");
 
-         Console.WriteLine($"A soma dos valores é {total}");
+         Console.WriteLine($"A soma dos valores é {resultado}");
+         Console.ReadKey();
       }
+      static void Subtracao()
+      {
+         Console.Clear();
+
+         Console.WriteLine("Primeiro valor: ");
+         float v1 = float.Parse(Console.ReadLine());
+
+         Console.WriteLine("Segundo valor: ");
+         float v2 = float.Parse(Console.ReadLine());
+
+         Console.WriteLine("");
+
+         float resultado = v1 - v2;
+         Console.WriteLine($"A subtração dos valores é {resultado}");
+         Console.ReadKey();
+      }
+
    }
+
 }
